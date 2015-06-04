5cbd04cbe7d1d1f643be122d73123bebd23ecb00
TimIJpenga <tim.ijpenga@student.hu.nl>
2015-06-04 19:22:35 +0200

    Update Main.java

--- Leonidas/scrumbag/Main.java
+++ Leonidas/scrumbag/Main.java
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-package scrumbag;
 
-public class Main {
-	private static Controller controller;
-
-	public static void main(String[] args) {
-
-		String accessToken = "CAAVvLje0a38BAH80qwafgbqAP2X1nSZAYTlWCnCpdwn1UJtb"
-				+ "cTOCPUg1YsGiJCzpT1nq1gx4ZBaC7ZCl5ssLdAei4Ql56UZBdEYzy7uVDnQD"
-				+ "oCSDOuWRDk8r76ZCglV4tWLh5ns0mdnNLcuMjcczC0PmnzWcnSVPcHmziHxq"
-				+ "xwdknTdjE5jKE8ZAPOEhKak8zLITef4Pb0CoHZAaZCkO11vo";
-
-		controller = new Controller();
-		String winner = controller.makeSuggestion(accessToken).getName();
-		System.out.println("suggestion: " + winner);
-	}
-}
-=======
 package scrumbag;
 
 import java.io.FileNotFoundException;
@@ -32,4 +14,3 @@ public class Main {
 		
 	}
 }
->>>>>>> 5c7e126b334cd885a5154cf42e7a4662abf935b2
